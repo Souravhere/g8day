0b1b264b34758dd46e35bafc3e9fd64dfa17c11f
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-05-01 21:13:46 +0530

    try to fix the vercel error

--- src/app/api/ai/route.js
+++ src/app/api/ai/route.js
@@ -5,8 +5,8 @@ const openai = new OpenAI({
   baseURL: 'https://openrouter.ai/api/v1',
   apiKey: process.env.OPENAI_API_KEY,
   defaultHeaders: {
-    'HTTP-Referer': '<YOUR_SITE_URL>',
-    'X-Title': '<YOUR_SITE_NAME>',
+    'HTTP-Referer': process.env.YOUR_SITE_URL,
+    'X-Title': process.env.YOUR_SITE_NAME,
   },
 });
 
